75d342c1ee5e8cbe2b0d9bd99a6e5a81af67f5f0
Andrei Buneyeu <yankeppey@gmail.com>
2013-08-30 16:07:08 +0300

    Minimoog waves & oscillator sync
    Oscillator sync feature added,
    all minimoog waves are implemented

--- MiniSynthLibrary/src/by/buneyeu/minisynth/oscillators/Oscillator.scala
+++ MiniSynthLibrary/src/by/buneyeu/minisynth/oscillators/Oscillator.scala
@@ -6,13 +6,11 @@ import scala.math.sin
 import by.buneyeu.minisynth.SampleRateDevice
 import by.buneyeu.minisynth.SampleProcessor
 
-class Oscillator(rate: Int) extends SampleRateDevice(rate) with SampleProcessor {
+class Oscillator(sampleRate: Int) extends SampleRateDevice(sampleRate) with SampleProcessor {
   val Tag = getClass.getSimpleName
 
-  val mFrequency: MutableFrequency = new MutableFrequency(rate)
-  var rads: Double = 0
-
-  val twopiRate: Double = 2 * Pi / rate
+  val mFrequency: MutableFrequency = new MutableFrequency(sampleRate)
+  var mRads: Double = 0
 
   def processSample(sample: Double) : Double = processSample
   
@@ -20,25 +18,66 @@ class Oscillator(rate: Int) extends SampleRateDevice(rate) with SampleProcessor 
     doSaw
   }
 
-  def doWave(f: Double => Double) : Double = {
-    rads += twopiRate * mFrequency.nextValue()
-    rads %=  2 * Pi
-    f(rads)
+  private def doWave(f: Double => Double): Double = {
+    mRads += 2 * Pi * mFrequency.nextValue() / sampleRate
+    val newRads = mRads % (2 * Pi)
+    if (newRads != mRads && synchronizedOscillator.isDefined) {
+      synchronizedOscillator.get.mRads = newRads
+    } 
+    mRads = newRads
+    f(mRads)
   }
   
-  def doSaw() = doWave(saw)
+  def resetPhase() = mRads = 0
+  
+  var synchronizedOscillator : Option[Oscillator] = None
+  
+  private def doSaw() = doWave(triangle)
 
+  def setPitch(pitch: Double) = mFrequency.setPitch(_)
   def setFreq = mFrequency.setFinalValue(_: Hz) 
   def setGlide = mFrequency.setGlide(_: Int)
 
-  def saw(rads: Double): Double = {
-    val phase = rads % (2 * Pi)
+  def triangle(phase: Double): Double = sawtoothTriangle(phase, 0)
 
-    if (phase <= Pi) {
-      1 - phase * 2 / Pi
-    } else {
-      phase * 2 / Pi - 3
-    }
+  def ascendingSawtooth(phase: Double): Double = {
+    //TODO prevent too sharp decline here
+    phase / Pi - 1
   }
 
+  def descendingSawtooth(phase: Double) = ascendingSawtooth(phase) * -1
+  
+  def sawtoothTriangle(phase: Double) : Double = sawtoothTriangle(phase, 0.1)
+  
+  /* sawtoothLevel = 1 => sawtooth-triangle wave == wave with sawtooth impulses
+   * sawtoothLevel = 0 => sawtooth-triangle wave == triangle wave 
+   */
+  private def sawtoothTriangle(phase: Double, sawtoothLevel: Double): Double = {
+    if (phase <= Pi) {
+      level(1 - sawtoothLevel * 2, -1, phase, Pi)
+    } else {
+      level(-1, 1, phase, Pi)
+    }
+  }
+  
+  private val wideRectangularDutyCycle = 35
+  def wideRectangular(phase: Double) = pulse(phase, wideRectangularDutyCycle)
+  
+  private val narrowRectangularDutyCycle = 15
+  def narrowRectangular(phase: Double) = pulse(phase, narrowRectangularDutyCycle)
+  
+  private val squareDutyCycle = 50
+  def square(phase: Double) = pulse(phase, squareDutyCycle)
+  
+  type Percent = Double
+  val Percents = 100
+  
+  private def pulse(phase: Double, dutyCycle: Percent) : Double = {
+    //TODO prevent too sharp decline here
+    if (phase <= 2 * Pi * dutyCycle / Percents)
+      1
+    else
+      -1
+  }
+  
 }
