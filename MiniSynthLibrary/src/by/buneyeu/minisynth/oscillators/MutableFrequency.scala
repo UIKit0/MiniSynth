74659919487c7deddf8df4147a2eb41782f3ca83
Andrei Buneyeu <yankeppey@gmail.com>
2013-08-30 16:05:54 +0300

    Pitch feature
    Pitch feature added to MutableFrequency

--- MiniSynthLibrary/src/by/buneyeu/minisynth/oscillators/MutableFrequency.scala
+++ MiniSynthLibrary/src/by/buneyeu/minisynth/oscillators/MutableFrequency.scala
@@ -6,6 +6,7 @@ import by.buneyeu.minisynth.SampleRateDevice
 class MutableFrequency(sampleRate: Int) extends SampleRateDevice(sampleRate) {
   val Tag = getClass.getSimpleName
 
+  
   var mStep : Int = 0
   var mFinalValue : Hz = 0
   var mNumSteps : Int = 0
@@ -13,18 +14,31 @@ class MutableFrequency(sampleRate: Int) extends SampleRateDevice(sampleRate) {
   var mStartValue : Hz = 0
   var mInc : Hz = 0
   
+  type Semitones = Double 
+  var mPitch : Semitones = 0
+  var mPitchMultiplicator: Double = 1
+  
   def setGlide(glide: Ms) = mGlide = glide
   
   def setFinalValue(freq: Hz) = {
     mStartValue = getValue
     mFinalValue = freq
     val ms = MsInSec / sampleRate
-    mInc = if (mGlide > 0) (mFinalValue - mStartValue) * ms / mGlide else 0
-    mNumSteps = (mGlide / ms).toInt 
+    mNumSteps = (mGlide / ms).toInt
+    mInc = if (mGlide > 0) (mFinalValue - mStartValue) / mNumSteps else 0
     mStep = 0
   }
+  
+  def setPitch(pitch: Double) = {
+    mPitch = pitch
+    mPitchMultiplicator = math.pow(SampleRateDevice.Semitone, mPitch)
+  }
 
-  def getValue: Hz = if (mGlide > 0) mStartValue + mInc * mStep else mFinalValue  
+  def getValue: Hz = {
+    val standardValue = if (mGlide > 0) mStartValue + mInc * mStep else mFinalValue
+    val pitchedValue = standardValue * mPitchMultiplicator
+    pitchedValue
+  }
   
   def nextValue(): Double = {
     val value = getValue
