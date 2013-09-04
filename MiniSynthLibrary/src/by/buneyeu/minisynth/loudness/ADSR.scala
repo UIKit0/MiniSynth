2ad1dead0ba6663ddd76c6301fa37d9658059320
Andrei Buneyeu <yankeppey@gmail.com>
2013-09-04 17:28:44 +0300

    LoudnessContour refactoring
    Separate class for ADSR contour was added to reuse it in MinimoogFilter

---
+++ MiniSynthLibrary/src/by/buneyeu/minisynth/loudness/ADSR.scala
@@ -0,0 +1,68 @@
+package by.buneyeu.minisynth.loudness
+
+import by.buneyeu.minisynth.NoteListener
+import by.buneyeu.minisynth.SampleRateDevice
+
+object State extends Enumeration {
+	type State = Value
+	val Attack, Decay, Sustain, Release = Value
+}
+
+class ADSR(sampleRate: Int) extends SampleRateDevice(sampleRate) with NoteListener {
+
+  import State._
+    
+  var state = Attack
+  
+  var attack: Ms = 0
+  var decay: Ms = 0
+  var normalizedSustain : Double = 0
+
+  var tFromLastState = 0d
+  
+  val MaxSustain = 10d
+  
+  def reset(attackIn: Ms, decayIn: Ms, sustainIn: Double /* 0-10 */ ) = {
+    attack = attackIn
+    decay = decayIn
+    normalizedSustain = sustainIn / MaxSustain
+  }
+
+  var lastLevel = 0d
+
+  def noteOn(note: Int) = ADSR.this.synchronized {
+    updateStateTo(Attack)
+    tFromLastState = lastLevel * attack
+  }
+
+  def noteOff(note: Int) = ADSR.this.synchronized {
+    updateStateTo(Release)
+  }
+
+  def updateStateTo(stateIn: State) = ADSR.this.synchronized {
+    tFromLastState = 0d
+    state = stateIn
+  }
+
+  def updateState() = ADSR.this.synchronized {
+    if (state == Attack && tFromLastState > attack)
+      updateStateTo(Decay)
+    else if (state == Decay && tFromLastState > decay)
+      updateStateTo(Sustain)
+  }
+
+  def update() : Double = {
+    tFromLastState += MsInSec.toDouble / sampleRate
+    updateState()
+    lastLevel = updateLoudness
+    lastLevel
+  }
+  
+  def updateLoudness =
+    state match {
+      case Attack => level(0, 1, tFromLastState, attack)
+      case Decay => level(1, normalizedSustain, tFromLastState, decay)
+      case Sustain => normalizedSustain
+      case Release => level(normalizedSustain, 0, tFromLastState, decay)
+    }
+}
--- MiniSynthLibrary/src/by/buneyeu/minisynth/loudness/LoudnessContour.scala
+++ MiniSynthLibrary/src/by/buneyeu/minisynth/loudness/LoudnessContour.scala
@@ -8,67 +8,11 @@ object LoudnessContour {
   val Tag = getClass getSimpleName
 }
 
-object State extends Enumeration {
-  type State = Value
-  val Attack, Decay, Sustain, Release = Value
-}
-
-class LoudnessContour(sampleRate: Int) extends SampleRateDevice(sampleRate) with NoteListener with SampleProcessor {
-  LoudnessContour
-
+class LoudnessContour(sampleRate: Int) extends ADSR(sampleRate) with SampleProcessor {
   import State._
-    
-  var state = Attack
-  
-  var attack: Ms = 0
-  var decay: Ms = 0
-  var normalizedSustain : Double = 0
-
-  var tFromLastState = 0d
-  
-  val MaxSustain = 10d
-  
-  def reset(attackIn: Ms, decayIn: Ms, sustainIn: Double /* 0-10 */ ) = {
-    attack = attackIn
-    decay = decayIn
-    normalizedSustain = sustainIn / MaxSustain
-  }
-
-  var lastLoudness = 0d
-
-  def noteOn(note: Int) = this.synchronized {
-    updateStateTo(Attack)
-    tFromLastState = lastLoudness * attack
-  }
-
-  def noteOff(note: Int) = this.synchronized {
-    updateStateTo(Release)
-  }
-
-  def updateStateTo(stateIn: State) = this.synchronized {
-    tFromLastState = 0d
-    state = stateIn
-  }
-
-  def updateState() = this.synchronized {
-    if (state == Attack && tFromLastState > attack)
-      updateStateTo(Decay)
-    else if (state == Decay && tFromLastState > decay)
-      updateStateTo(Sustain)
-  }
 
   override def processSample(sample: Double): Double = {
-    tFromLastState += MsInSec.toDouble / sampleRate
-    updateState()
-    lastLoudness = updateLoudness
-    sample * lastLoudness
+    val loudness = update();
+    sample * loudness
   }
-
-  def updateLoudness =
-    state match {
-      case Attack => level(0, 1, tFromLastState, attack)
-      case Decay => level(1, normalizedSustain, tFromLastState, decay)
-      case Sustain => normalizedSustain
-      case Release => level(normalizedSustain, 0, tFromLastState, decay)
-    }
 }
