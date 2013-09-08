c6a21296075125595a84d347b90e937ba67fcc20
Andrei Buneyeu <yankeppey@gmail.com>
2013-09-08 11:52:31 +0300

    IIRFilter refactoring
    little IIRFilter refactoring

--- MiniSynthLibrary/src/by/buneyeu/minisynth/filters/IIRFilter.scala
+++ MiniSynthLibrary/src/by/buneyeu/minisynth/filters/IIRFilter.scala
@@ -4,20 +4,20 @@ import by.buneyeu.minisynth.SampleProcessor
 import by.buneyeu.minisynth.SampleRateDevice
 
 //TODO check it carefully and refactor hard in scala way
-class IIRFilter(sampleRate: Int, a: Array[Double], b: Array[Double]) extends SampleRateDevice(sampleRate) with SampleProcessor {
+class IIRFilter(a: Array[Double], b: Array[Double]) extends SampleProcessor {
   require(b.length > 0, "To create IIR filter you should define at least one b coeff!")
 
   private val x = Array.fill(b.length)(0.0)
   private val y = Array.fill(a.length)(0.0)
-  
-  def processSample(xIn: Double) : Double = {
+
+  def processSample(xIn: Double): Double = {
     delay(x, xIn)
     val yOut = sum(mult(b, x)) + sum(mult(a, y))
     delay(y, yOut)
     yOut
   }
 
-  def delay(arr: Array[Double], newValue: Double) = {
+  private def delay(arr: Array[Double], newValue: Double) = {
     //very stupid delay implementation with O(n) complexity,
     //should use LinkedList or something else instead
     for (i <- 1 until x.length) {
@@ -25,14 +25,14 @@ class IIRFilter(sampleRate: Int, a: Array[Double], b: Array[Double]) extends Sam
     }
     arr(0) = newValue
   }
-  
-  def sum(arr: Array[Double]) : Double = arr.reduceLeft(_ + _)
-  def mult(arr1: Array[Double], arr2: Array[Double]) : Array[Double] = {
+
+  private def sum(arr: Array[Double]): Double = arr.reduceLeft(_ + _)
+
+  private def mult(arr1: Array[Double], arr2: Array[Double]): Array[Double] = {
     require(arr1.length == arr2.length, "Arrays should have the same length!")
     val multArr = Array.fill(arr1.length)(0.0)
-    for(i <- 0 until arr1.length){
+    for (i <- 0 until arr1.length)
       multArr(i) = arr1(i) * arr2(i)
-    }
     multArr
   }
 }
